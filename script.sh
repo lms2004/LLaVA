72788ba6b077a5b1d2ab63d94814faebb7756047
Your Name <you@example.com>
2025-06-02 21:31:38 +0800

    add: 添加 sglang worker 和 model_worker 命令

--- script.sh
+++ script.sh
@@ -25,7 +25,7 @@ conda activate llava
 pip install --upgrade pip  # enable PEP 660 support
 pip install -e .
 pip install gradio
-
+pip install "sglang[all]"
 
 # 运行
 python -m llava.serve.controller --host 0.0.0.0 --port 10000
@@ -33,8 +33,11 @@ python -m llava.serve.controller --host 0.0.0.0 --port 10000
 python -m llava.serve.gradio_web_server --controller http://localhost:10000 --model-list-mode reload
 
 
-# 单独安装
-pip install "sglang[all]"
-
 # 双卡
-CUDA_VISIBLE_DEVICES=0,1 python3 -m sglang.launch_server --model-path ./models/liuhaotian/llava-v1.5-13b --tokenizer-path ./models/llava-hf/llava-1.5-13b-hf --port 30000 --tp 2
+CUDA_VISIBLE_DEVICES=0,1 python3 -m sglang.launch_server --model-path ./models/liuhaotian/llava-v1.5-7b --tokenizer-path ./models/llava-hf/llava-1.5-7b-hf --port 30000 --tp 2
+
+# llava 的 worker
+python -m llava.serve.sglang_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --sgl-endpoint http://127.0.0.1:30000
+
+# 实际推理的worker
+python -m llava.serve.model_worker --host 0.0.0.0 --controller http://localhost:10000 --port 40000 --worker http://localhost:40000 --model-path ./models/liuhaotian/llava-v1.5-7b
